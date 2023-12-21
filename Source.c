3a1d3449e18f3c25230c230b933e5d480f023bba
AlexeyMikhylov <alexeymikhylov02@gmail.com>
2023-12-21 12:55:12 +0300

    Add project files.

---
+++ Source.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    FILE* pData = fopen("file.txt", "r");
+    int counter = 1;
+
+    while (!feof(pData)) {
+        if (fgetc(pData) == '\n') {
+            counter++;
+        }
+    }
+
+    printf("there is %d rows in file", counter);
+    fclose(pData);
+    return 0;
+}
